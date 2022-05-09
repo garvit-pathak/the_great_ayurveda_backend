4527820bb0a9460f7cda1d482fe1f58e71858034
Your Name <you@example.com>
2022-05-09 10:43:11 +0530

    disease

--- controller/disease.controller.js
+++ controller/disease.controller.js
@@ -1,46 +1,83 @@
 
-// const diseaseM=require('../model/disease.model');
-// const path = require('path');
-// const { Storage } = require('@google-cloud/storage');
-// let bucketName = "gs://ayurveda-d6cac.appspot.com"
+const diseaseM=require('../model/disease.model');
+const path = require('path');
+const { Storage } = require('@google-cloud/storage');
 
-// const storage = new Storage({
-//     keyFilename: "serviceFirebaseStorage.json"
-// });
+let bucketName = "gs://ayurveda-d6cac.appspot.com"
 
-// const uploadFile = async (filename) => {
+const storage = new Storage({
+    keyFilename: "serviceFirebaseStorage.json"
+});
 
-//     await storage.bucket(bucketName).upload(filename, {
-//         gzip: true,
-//         metadata: {
-//             metadata: {
-//                 firebaseStorageDownloadTokens: "disease-image"
-//             }
-//         },
-//     });
+const uploadFile = async (filename) => {
 
-//     console.log(`${filename} uploaded to ${bucketName}.`);
-// }
+    await storage.bucket(bucketName).upload(filename, {
+        gzip: true,
+        metadata: {
+            metadata: {
+                firebaseStorageDownloadTokens: "image"
+            }
+        },
+    });
 
-// exports.Add=(request,response)=>{
-//     let a= request.body.name;
-//     let b= request.body.causes;
-//     let c= request.body.homeRemedies ;
-//     let d= request.body.yogaLink;
-//     let e = request.body.yogaThumbnail;
-//     let f= request.file.filename;
-//     let g= request.body.keyword ;
-    
-   
-    
-//     diseaseM.create({ name: a, causes: b, homeRemedies: c, yogaLink: d, yogaThumbnail: e, image: f, keyword: g })
-//     .then(result => {
-//         uploadFile(
-//             path.join(__dirname, "../", "public/images/") + request.file.filename
-//         );
-//         return response.status(201).json(result);
-//     }).catch(err => {
-//         console.log(err);
-//         return response.status(500).json({ error: 'Cannot Added' });
-//     });
-// }
+    console.log(`${filename} uploaded to ${bucketName}.`);
+}
+
+exports.Add=(request,response)=>{
+    let name= request.body.name;
+    let causes= request.body.causes;
+    let homeRemedies = request.body.homeRemedies ;
+    let yogaLink= request.body.yogaLink;
+    let yogaThumbnail= request.body.yogaThumbnail;
+    let image= 'https://firebasestorage.googleapis.com/v0/b/ayurveda-d6cac.appspot.com/o/' + request.file.filename + '?alt=media&token=saved-image';
+    let keyword= request.body.keyword ;
+    let category= request.body.category;
+     
+    diseaseM.create({ name: name, causes: causes, homeRemedies: homeRemedies, yogaLink: yogaLink, yogaThumbnail: yogaThumbnail, image: image, keyword: keyword ,category:category})
+    .then(result => {
+        uploadFile(
+            path.join(__dirname, "../", "public/images/") + request.file.filename
+        );
+        return response.status(201).json(result);
+    }).catch(err => {
+        console.log(err);
+        return response.status(500).json({ error: 'Addition not possible' });
+    });
+}
+exports.Review = async (request, response) => {
+    console.log(request.body);
+    let uId = request.body.uId;
+    let pId = request.body.pId;
+    let reviewText = request.body.reviewText;
+
+    let review = await diseaseM.findOne({ _id: pId });
+    let he = {
+        _id: uId,
+        reviewText: reviewText
+    }
+    review.reviewerDetail.push(he);
+    review.save().then(result => {
+        return response.status(200).json(result);
+    }).catch(err => {
+        console.log(err);
+        return response.status(500).json({ error: 'Something went wrong' });
+    });
+}
+ exports.Delete=(request,response)=>{
+   diseaseM.deleteOne({_id:request.body.id}).then(result=>{
+       return response.status(200).json(result);
+   }).catch(err=>{
+       console.log(err);
+       return response.status(500).json({error:'deletion not done'})
+   })
+ }
+ exports.ViewAll = (request, response) => {
+    diseaseM.find().then(result => {
+        return response.status(200).json(result);
+    }).catch(err => {
+        console.log(err);
+        return response.status(500).json({ error: 'Something went wrong' });
+    });
+}
+
+ 
