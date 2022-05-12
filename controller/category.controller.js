cf41ab13cf45695c101e73d7e3d75263ebbcb9a9
Your Name <you@example.com>
2022-05-12 18:40:26 +0530

    jaya7

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -5,7 +5,7 @@ exports.Add=(request,response)=>{
     let a=request.body.name;
   
     categoryM.create({name:a}).then(result=>{
-        return response.status(500).json({result:result,message:'Added'});
+        return response.status(200).json(result);
         
     }).catch(err=>{
         console.log(err);
@@ -16,7 +16,7 @@ exports.Add=(request,response)=>{
 exports.View=(request,response)=>{
     
     categoryM.find().then(result=>{
-        return response.status(500).json({result:result});
+        return response.status(200).json(result);
         
     }).catch(err=>{
         console.log(err);
@@ -27,7 +27,7 @@ exports.View=(request,response)=>{
 exports.ViewOne=(request,response)=>{
     let a=request.body.id;
     categoryM.findOne({_id:a}).then(result=>{
-        return response.status(500).json({result:result});
+        return response.status(200).json(result);
         
     }).catch(err=>{
         console.log(err);
@@ -37,7 +37,7 @@ exports.ViewOne=(request,response)=>{
 exports.Delete=(request,response)=>{
     let a=request.body.id;
     categoryM.deleteOne({_id:a}).then(result=>{
-        return response.status(500).json({result:result});
+        return response.status(200).json(result);
         
     }).catch(err=>{
         console.log(err);
@@ -49,7 +49,7 @@ exports.Update=(request,response)=>{
     let a=request.body.name;
     let b=request.body.id;
     categoryM.updateOne({_id:b},{$set:{name:a}}).then(result=>{
-        return response.status(500).json({result:result});
+        return response.status(200).json(result);
         
     }).catch(err=>{
         console.log(err);
