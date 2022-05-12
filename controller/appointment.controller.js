7ca17fdc355ef71a0da0c3bafb209a56e7f272bd
garvit pathak <95516960+garvit-pathak@users.noreply.github.com>
2022-05-12 11:39:50 +0530

    Update appointment.controller.js

--- controller/appointment.controller.js
+++ controller/appointment.controller.js
@@ -9,42 +9,44 @@ exports.BookAppointment = (request, response) => {
     let mobile = request.body.mobile;
     let userId = request.body.userId;
     let doctorId = request.body.doctorId;
-    let date=new Date();
-    let b=date.getDate();
-    let c=date.getMonth() +1;
-    let d=date.getFullYear();
-    let currentDate=b+'/'+c+'/'+d
+    let date = new Date();
+    let b = date.getDate();
+    let c = date.getMonth() + 1;
+    let d = date.getFullYear();
+    let currentDate = b + '/' + c + '/' + d
     console.log(currentDate);
 
-    appointmentM.create({ patientName: patientName, age: age, disease: disease, mobile: mobile, userId: userId, doctor: doctorId,date:currentDate }).then(result => {
+    appointmentM.create({ patientName: patientName, age: age, disease: disease, mobile: mobile, userId: userId, doctor: doctorId, date: currentDate }).then(result => {
         var options = {
             authorization: "FtQi9Z8SXlC5rq1VdNjsKREuO7wWTmnc6zvbI0eJHYLfohMAUxL5mucn6aw1PpNosir4G8gyJzRFEeYj",
-            message: 'Dear customer we have received your appointment request Date:'+currentDate+' soon doctor will contact you , Team:The Great Ayurveda',
+            message: 'Dear customer we have received your appointment request Date:' + currentDate + ' soon doctor will contact you , Team:The Great Ayurveda',
             numbers: [mobile]
         }
         fastTwoSms.sendMessage(options).then(result => {
+            return response.status(200).json(result);
             console.log(result);
-        
+
         }).catch(err => {
             console.log(err);
+            return response.status(500).json({ message: "error" });
         });
-   
-    
-    
+
+    })
+}
+
 
 
 
 exports.DoctorResponseAccept = (request, response) => {
-    appointmentM.updateOne({_id:request.body.aId},{$set:{apointmentStatus:'Accepted'}}).then(result=>{
-        if(result.modifiedCount && result.matchedCount){
-            return response.status(200).json({result:result,message:'Appointment accepted'});
+    appointmentM.updateOne({ _id: request.body.aId }, { $set: { apointmentStatus: 'Accepted' } }).then(result => {
+        if (result.modifiedCount && result.matchedCount) {
+            return response.status(200).json({ result: result, message: 'Appointment accepted' });
+        } else {
+            return response.status(404).json({ result: result, message: 'Appointment not accepted' });
         }
-        else{
-            return response.status(404).json({result:result,message:'Appointment not accepted'});
-        }
-        
 
-    }).catch(err=>{
+
+    }).catch(err => {
         console.log(err);
         return response.status(500).json({ error: 'Not Updated' });
 
@@ -52,22 +54,21 @@ exports.DoctorResponseAccept = (request, response) => {
 }
 
 exports.ViewAppointment = (request, response) => {
-    appointmentM.find().then(result=>{
+    appointmentM.find().then(result => {
         return response.status(201).json(result);
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err);
-        return response.status(500).json({error:'Cannot fetch data'});
+        return response.status(500).json({ error: 'Cannot fetch data' });
     })
 }
 exports.DoctorResponseReject = (request, response) => {
-    appointmentM.updateOne({_id:request.body.aId},{$set:{apointmentStatus:'Rejected'}}).then(result=>{
-        if(result.modifiedCount && result.matchedCount){
-            return response.status(200).json({result:result,message:'Appointment Rejected'});
+    appointmentM.updateOne({ _id: request.body.aId }, { $set: { apointmentStatus: 'Rejected' } }).then(result => {
+        if (result.modifiedCount && result.matchedCount) {
+            return response.status(200).json({ result: result, message: 'Appointment Rejected' });
+        } else {
+            return response.status(404).json({ result: result, message: 'Appointment not rejected' });
         }
-        else{
-            return response.status(404).json({result:result,message:'Appointment not rejected'});
-        }
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err);
         return response.status(500).json({ error: 'Not Updated' });
 
