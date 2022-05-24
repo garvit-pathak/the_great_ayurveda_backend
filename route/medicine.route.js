5d6676296e997b742a0ec5af03bc7b7144a768c6
faizan-lala <faizankhaan6267448042@gmail.com>
2022-05-24 14:21:43 +0530

    jaya

--- route/medicine.route.js
+++ route/medicine.route.js
@@ -4,23 +4,27 @@ const medicineControl = require("../controller/medicine.controller");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: "public/images",
-  filename: (request, file, cb) => {
-    cb(null, Date.now() + "_" + file.originalname);
-  },
+    destination: "public/images",
+    filename: (request, file, cb) => {
+        cb(null, Date.now() + "_" + file.originalname);
+    },
 });
 
 const upload = multer({ storage: storage });
 
 medicineRouter.post('/add',
-                    upload.single('image'),medicineControl.Add);
-medicineRouter.post('/review',medicineControl.Review);
-medicineRouter.get('/viewall',medicineControl.ViewAll);
-medicineRouter.post('/viewbypro',medicineControl.ViewByProduct);
-medicineRouter.post('/viewbycat',medicineControl.ViewByCat);
-medicineRouter.post('/delete',medicineControl.Delete);
-medicineRouter.post('/update',upload.single('image'),medicineControl.Update);
-medicineRouter.post('/search',medicineControl.viewByKeyword);
-medicineRouter.post('/excel',medicineControl.ExcelSave);
-module.exports=medicineRouter;
+    upload.single('image'), medicineControl.Add);
+medicineRouter.get('/viewall', medicineControl.ViewAll);
+medicineRouter.post('/viewbypro', medicineControl.ViewByProduct);
+medicineRouter.post('/viewbycat', medicineControl.ViewByCat);
 
+medicineRouter.post('/delete', medicineControl.Delete);
+medicineRouter.post('/search', medicineControl.viewByKeyword);
+
+medicineRouter.post('/update', upload.single('image'), medicineControl.Update);
+
+medicineRouter.post('/review', medicineControl.Review);
+
+medicineRouter.post('/excel', medicineControl.ExcelSave);
+
+module.exports = medicineRouter;
